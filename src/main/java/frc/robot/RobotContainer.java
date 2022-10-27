52f4a05198653b7a8b7630680708f7ee108c6b63
Husky Peverell <30388905+HuskyPeverell@users.noreply.github.com>
2022-10-26 19:44:14 -0700

    Clean up and refactor
    Rename most occurrences of example to internals

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.InternalsCommand;
+import frc.robot.subsystems.InternalsSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -18,9 +18,9 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final InternalsSubsystem m_internalsSubsystem = new InternalsSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final InternalsCommand m_internalsCommand = new InternalsCommand(m_internalsSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -42,7 +42,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    // An InternalsCommand will run in autonomous
+    return m_internalsCommand;
   }
 }
